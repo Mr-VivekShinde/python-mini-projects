3293b76a8b1644699811ea89ad363ba03c487a3d
Vivek Shinde <vivshinde16@gmail.com>
2025-11-23 22:03:16 +0100

    include hour and minutes

--- Countdown_timer/code3.py
+++ Countdown_timer/code3.py
@@ -1,6 +1,11 @@
 import time
+print("set your own countdown timer")
 set_time=int(input("Enter time in seconds :"))
 for i in range(set_time,0,-1):
-    print(i)
+    hour=int(i/3600)
+    min=int(i/60) % 60
+    sec=int(i%60)
+    
+    print(f"{hour:02}:{min:02}:{sec:02}")
     time.sleep(1)
 print("TIMES'S UP!")
